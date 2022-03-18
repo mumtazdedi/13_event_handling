c2c80b83db6419f02ae80806e5ac46520b9fe7b0
mumtazdedi <mumtazdedi@gmail.com>
2022-03-18 18:36:53 +0700

    Section 13 event handling alterra reactJS

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.css';
+
+const Index = () => {
+    return (
+        <div>
+            <h1 className={style.title}>TODO APP</h1>
+        </div>
+    );
+}
+
+export default Index;
